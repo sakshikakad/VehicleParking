2ec228d0fbe4a342b4584dc6bad75f74d6adaf5d
Kakad, Akshay <82809356+kakadakshay@users.noreply.github.com>
2022-12-20 11:09:36 +0530

    Searchbar on home screen

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,14 +1,39 @@
-import { StyleSheet, Text, View, FlatList, Button, Alert } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Button,
+  Alert,
+  clicked,
+  TextInput,
+  Keyboard,
+} from "react-native";
 import React, { useState, useEffect } from "react";
 import { CustomerService } from "../Service/CustomerService";
+import { Feather, Entypo } from "@expo/vector-icons";
 
 export default function HomeScreen({ navigation }) {
+  // this variable is used to store the all customer data
   const [customerData, setCustomerData] = useState([]);
+  // to filter that data
+  const [filteredData, setFilteredData] = useState([]);
 
+  // useEffect to call the service to get all data while loading the page
   useEffect(() => {
     setCustomerData(CustomerService.getAllCustomerData());
+    setFilteredData(CustomerService.getAllCustomerData());
   }, []);
 
+  // function used to filter the data
+  const onSearchTextChange = (txt) => {
+    var filteredList = customerData.filter((item) => {
+      return item.rollNo.indexOf(txt) > -1;
+    });
+    setFilteredData(filteredList);
+  };
+
+  // ui of item in the list
   const renderItem = ({ item }) => (
     <View style={styles.listItem}>
       <View style={styles.listDetailsSection}>
@@ -29,6 +54,7 @@ export default function HomeScreen({ navigation }) {
     </View>
   );
 
+  // separator line for list items
   const separator = () => {
     return (
       <View
@@ -43,8 +69,30 @@ export default function HomeScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
+      <View
+        style={[
+          clicked ? styles.searchBar__clicked : styles.searchBar__unclicked,
+          styles.searchMargin,
+        ]}
+      >
+        {/* search Icon */}
+        <Feather
+          name="search"
+          size={20}
+          color="black"
+          style={{ marginLeft: 1 }}
+        />
+
+        {/* Input field */}
+        <TextInput
+          style={styles.input}
+          placeholder="Search"
+          onChangeText={onSearchTextChange}
+        />
+      </View>
+
       <FlatList
-        data={customerData}
+        data={filteredData}
         renderItem={renderItem}
         keyExtractor={(item) => item._id}
         ItemSeparatorComponent={separator}
@@ -57,17 +105,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "stretch",
+    alignItems: "center",
     justifyContent: "center",
   },
   listItem: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-evenly",
-    padding:5
+    padding: 5,
   },
-  listAmountSection:{
-    paddingVertical:5
+  listAmountSection: {
+    paddingVertical: 5,
   },
   listNameSection: {
     flexDirection: "row",
@@ -82,5 +130,30 @@ const styles = StyleSheet.create({
     paddingRight: 10,
     paddingLeft: 10,
     width: 200,
+  },
+  searchBar__unclicked: {
+    padding: 10,
+    flexDirection: "row",
+    width: "95%",
+    backgroundColor: "#d9dbda",
+    borderRadius: 15,
+    alignItems: "center",
+  },
+  searchBar__clicked: {
+    padding: 10,
+    flexDirection: "row",
+    width: "80%",
+    backgroundColor: "#d9dbda",
+    borderRadius: 15,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  input: {
+    fontSize: 20,
+    marginLeft: 10,
+    width: "90%",
+  },
+  searchMargin: {
+    marginBottom: 10,
   },
 });
--- Service/CustomerService.js
+++ Service/CustomerService.js
@@ -22,6 +22,41 @@ export class CustomerService {
         amount: 120,
         __v: 0,
       },
+      {
+        _id: "6393ea6262dda1a2b8cb2f15",
+        rollNo: "11",
+        name: "Akshay Kakad",
+        amount: 120,
+        __v: 0,
+      },
+      {
+        _id: "6393ea6262dda1a2b8cb2f16",
+        rollNo: "12",
+        name: "Deepika Kakad",
+        amount: 120,
+        __v: 0,
+      },
+      {
+        _id: "6393ea6262dda1a2b8cb2f17",
+        rollNo: "13",
+        name: "Pratik Lahane",
+        amount: 120,
+        __v: 0,
+      },
+      {
+        _id: "6393ea6262dda1a2b8cb2f18",
+        rollNo: "14",
+        name: "Rutuja Lahane",
+        amount: 120,
+        __v: 0,
+      },
+      {
+        _id: "6393ea6262dda1a2b8cb2f19",
+        rollNo: "15",
+        name: "Seema Kakad",
+        amount: 120,
+        __v: 0,
+      },
     ];
 
     return dummyCustomerData;
